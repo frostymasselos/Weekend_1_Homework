4ec0b40a093cad3b89d0f3c969223028bf6e0341
Ryan <ryanfrost2015@gmail.com>
2019-10-20 22:24:30 +0100

    First commit

---
+++ pet_shop.rb
@@ -0,0 +1,77 @@
+def pet_shop_name (instance_variable)
+  p instance_variable[:name]
+end
+
+def total_cash (instance_variable)
+  instance_variable[:admin][:total_cash]
+end
+
+def add_or_remove_cash__add (instance_variable, number)
+  instance_variable[:admin][:total_cash] += number
+end
+
+def add_or_remove_cash__remove (instance_variable, number)
+  instance_variable[:admin][:total_cash] += number
+end
+
+def pets_sold(instance_variable)
+  instance_variable[:admin][:pets_sold]
+end
+
+def increase_pets_sold(instance_variable, number)
+  instance_variable[:admin][:pets_sold] += number
+end
+
+def stock_count (instance_variable)
+  instance_variable[:pets].length
+end
+
+# def pets_by_breed (pet_shop, breed_type)
+#
+# selected_pets = []
+#
+#   for pet in pet_shop[:pets]
+#     if pet[:breed] = breed_type
+#       selected_pets.push(pet[:breed])
+#     end
+#   end
+# return selected_pets
+# end
+
+# you want it to circe all pets [:breed] and add an element to an array
+# when you pass in the result, which must be an array, when you .count it - it gives 2
+
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] = pet_name
+      return pet
+    end
+  end
+  return nil
+end
+
+# def remove_pet_by_name
+#
+# end
+
+# def stock_count (pet_shop)
+#   instance_variable[:pets].push()
+#   instance_variable[:pets].count
+# end
+
+def customer_cash (customer_1)
+  return customer_1[:cash]
+end
+
+# def remove_customer_cash (customer_1, money)
+#   return customer_1[:money] -= money
+#   # You have to replace Alice's cash
+# end
+
+def customer_pet_count (first_customer)
+  return first_customer[:pets].count
+end
+
+def add_pet_to_customer (first_customer, new_pet)
+  return first_customer[:pets].unshift(new_pet)
+end
